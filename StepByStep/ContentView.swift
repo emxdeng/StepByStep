954ba559ab0b5cb8a96f0b7bff41afbb5580026d
Emily Deng <emily.x.deng@student.uts.edu.au>
2023-05-07 19:44:58 +1000

    Initial Commit

---
+++ StepByStep/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StepByStep
+//
+//  Created by Emily Deng on 7/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StepByStep/StepByStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepByStepApp.swift
+//  StepByStep
+//
+//  Created by Emily Deng on 7/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepByStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
