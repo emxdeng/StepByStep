2a73c6362530fbdacdf78465c7f21c70636fbdac
Zzun98 <sunnylucie98@gmail.com>
2023-05-09 09:08:39 +1000

    First commit of add-goal-screens-3

--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -3,24 +3,276 @@
 //  StepByStep
 //
 //  Created by Emily Deng on 7/5/2023.
-//
+//  Modified by Sunjun (Lucie) Kwak on 8/5/2023.
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct CustomColor {
+    static let textColor = Color("TextColor")
+    static let gray = Color("Gray")
+    static let lightOrange = Color("LightOrange")
+    static let habitColor1 = Color("FAC088")
+    static let habitColor2 = Color("F29094")
+    static let habitColor3 = Color("A6CFF0")
+    static let habitColor4 = Color("F08461")
+    static let habitColor5 = Color("91C483")
+    static let habitColor6 = Color("F08675")
+    static let habitColor7 = Color("ECC268")
+    static let habitColor8 = Color("A7C86E")
+    static let habitColor9 = Color("7DA3D6")
+    static let habitColor10 = Color("9784BD")
+    static let habitColor11 = Color("EC648A")
+    static let habitColor12 = Color("F28D39")
+    static let habitColor13 = Color("77C7CC")
+    static let habitColor14 = Color("BABEC9")
+    static let habitColor15 = Color("B9CF4D")
+    static let habitColor16 = Color("B83325")
+    static let habitColor17 = Color("EDC33E")
+    static let habitColor18 = Color("52905B")
+    static let habitColor19 = Color("247CB9")
+    static let habitColor20 = Color("775C45")
+    static let habitColor21 = Color("D6746B")
+    static let habitColor22 = Color("EC928B")
+    static let habitColor23 = Color("E6A7B4")
+    static let habitColor24 = Color("F1B2A4")
+    static let habitColor25 = Color("EC987E")
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @State private var hasDueDate = true
+    @State private var selectedDate = Date()
+    @State private var habitText = ""
+    @State private var selectedHabitColor: Color? = nil
+    
+    var body: some View {
+        
+        // Code for title of this page
+        List {
+            Text("Now let's set the specifics of\nyour goal and habit :)")
+                .font(.system(size: 22))
+                .fontWeight(.bold)
+                .foregroundColor(CustomColor.textColor)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+            }
+        
+        // Code for choosing the goal the habit is part of
+        List {
+            VStack {
+                Text("My goal")
+                    .font(.system(size: 22))
+                    .fontWeight(.regular)
+                    .foregroundColor(CustomColor.textColor)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .alignmentGuide(.leading) { _ in 0 }
+                DisclosureGroup(/*@START_MENU_TOKEN@*/"Group"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+                }
+            }
+        }
+        
+        // Code for setting the due date of the goal
+        List {
+            VStack {
+                HStack{
+                    Text("Due date")
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
+                        .foregroundColor(hasDueDate ? CustomColor.textColor : CustomColor.gray)
+                        .multilineTextAlignment(.leading)
+                    Text("No due date")
+                        .fontWeight(.regular)
+                        .foregroundColor(hasDueDate ? CustomColor.textColor : CustomColor.gray)
+                        .font(.system(size: 16))
+                    Toggle(isOn: $hasDueDate) {
+                        Text("")
+                    }
+                }
+                if hasDueDate {
+                    DatePicker(selection: $selectedDate, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                        .background(CustomColor.lightOrange)
+                    
+                }
+            }
+        }
+        
+        // Code for 'helpful habit' that the user will input
+        List {
+            VStack {
+                Text("Helpful habit")
+                    .font(.system(size: 22))
+                    .fontWeight(.regular)
+                    .foregroundColor(CustomColor.textColor)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .alignmentGuide(.leading) { _ in 0 }
+                TextField("Write your habit here...", text: $habitText)
+                    .background(selectedHabitColor)
+            }
+        }
+        
+        // Code for habit color palette
+        List {
+            VStack {
+                Text("Habit colour")
+                    .font(.system(size: 22))
+                    .fontWeight(.regular)
+                    .foregroundColor(CustomColor.textColor)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .alignmentGuide(.leading) { _ in 0 }
+                HStack {
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor1
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor1)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor2
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor2)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor3
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor3)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor4
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor4)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor5
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor5)
+                    .clipShape(Circle())
+                }
+                HStack {
+                    Button("") {
+                        selectedHabitColor = CustomColor.habitColor6
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor6)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor7)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor8)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor9)
+                    .clipShape(Circle())
+                    
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor10)
+                    .clipShape(Circle())
+                }
+                HStack {
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor11)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor12)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor13)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor14)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor15)
+                    .clipShape(Circle())
+                }
+                HStack {
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor21)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor22)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor23)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor24)
+                    .clipShape(Circle())
+                    Button("") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(CustomColor.habitColor25)
+                    .clipShape(Circle())
+                }
+            }
+        }
+        
+        // Code for weekly calendar
+        List {
+            
+        }
+        
+        // Code for setting time
+        
+        // Code for setting amount of time or number of times per day
+        
+        // Code for 'save' button
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
