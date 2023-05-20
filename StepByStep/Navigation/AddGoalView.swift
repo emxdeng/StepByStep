7c13f0c06155d90b51023cae75a55b0997a33b0c
Emily Deng <emily.x.deng@student.uts.edu.au>
2023-05-20 22:59:05 +1000

    Update habits dynamically, add navigation

--- StepByStep/Navigation/AddGoalView.swift
+++ StepByStep/Navigation/AddGoalView.swift
@@ -9,142 +9,145 @@ import SwiftUI
 import Foundation
 
 struct AddGoalView: View {
-    
-    //Properties
     var body: some View {
-        
-        //View
-        VStack {
-        
-            Spacer()
-            
+        NavigationView {
             VStack {
+                Spacer().frame(height:16)
+                    
                 Text("Do you have something big you want to achieve?")
                     .font(.custom("Skia", size: 20))
-            }.padding()
+                    .offset(x:-10)
+                
+                Spacer().frame(height:50)
+                
+                NavigationLink(
+                    destination: AddHabitsView(selectedButton: "Staying fit")) {
+                        Button(action: {
+                        }, label: {
+                            HStack {
+                                Spacer()
+                                Image("dumbbell@4x")
+                                    .resizable()
+                                    .frame(width: 50, height: 30)
+                                Spacer()
+                                Text("Staying fit")
+                                    .font(.callout)
+                                    .foregroundColor(.black)
+                                Spacer()
+                            }
+                        }).buttonStyle(RoundedButtonStyle())
+                    }
 
-            
-            VStack {
-                
-                //Staying Fit
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("dumbbell@4x")
-                            .resizable()
-                            .frame(width: 50, height: 30)
-                        Spacer()
-                        Text("Staying fit")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
+
                 //Be a morning person
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("sun@4x")
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                        Spacer()
-                        Text("Be a morning person")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
+                NavigationLink(destination: AddHabitsView(selectedButton: "Be a morning person")) {
+                    Button(action: {
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Image("sun@4x")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                            Spacer()
+                            Text("Be a morning person")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                    }).buttonStyle(RoundedButtonStyle())
+                }
+
                 //Learn something new
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("book@4x")
-                            .resizable()
-                            .frame(width: 30, height: 40)
-                        Spacer()
-                        Text("Learn something new")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
+                NavigationLink(destination: AddHabitsView(selectedButton: "Learn something new")) {
+                    Button(action: {
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Image("book@4x")
+                                .resizable()
+                                .frame(width: 30, height: 40)
+                            Spacer()
+                            Text("Learn something new")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                    }).buttonStyle(RoundedButtonStyle())
+                }
+
+
                 //Control my budget
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("money@4x")
-                            .resizable()
-                            .frame(width: 30, height: 40)
-                        Spacer()
-                        Text("Control my budget")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
+                NavigationLink(destination: AddHabitsView(selectedButton: "Control my budget")) {
+                    Button(action: {
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Image("money@4x")
+                                .resizable()
+                                .frame(width: 30, height: 40)
+                            Spacer()
+                            Text("Control my budget")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                    }).buttonStyle(RoundedButtonStyle())
+                }
+
+
                 //Stay mentally healthy
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("locked heart@4x")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                        Spacer()
-                        Text("Stay mentally healthy")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
+                NavigationLink(destination: AddHabitsView(selectedButton: "Stay mentally healthy")) {
+                    Button(action: {
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Image("locked heart@4x")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                            Spacer()
+                            Text("Stay mentally healthy")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                    }).buttonStyle(RoundedButtonStyle())
+                }
+
+
                 //Be more productive
-                Button(action: {
-                    
-                }, label: {
-                    HStack{
-                        Spacer()
-                        Image("cogwheel@4x")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                        Spacer()
-                        Text("Be more productive")
-                            .font(.callout)
-                            .foregroundColor(.black)
-                        Spacer()
-                    }
-                }).buttonStyle(RoundedButtonStyle())
-                
-                
-                
+                NavigationLink(destination: AddHabitsView(selectedButton: "Be more productive")) {
+                    Button(action: {
+
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Image("cogwheel@4x")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                            Spacer()
+                            Text("Be more productive")
+                                .font(.callout)
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                    }).buttonStyle(RoundedButtonStyle())
+                }
+
+
+                Spacer()
+
+
             }
-            
-            Spacer()
-            
-            
+                .padding()
+                .navigationTitle("Add New Goal")
+
+
+
         }
-        .padding()
-        
-        
     }
-    
-    //Methods
-    
-    
+
 }
+
 
 struct AddGoalView_Previews: PreviewProvider {
     static var previews: some View {
@@ -152,3 +155,4 @@ struct AddGoalView_Previews: PreviewProvider {
             .previewDevice("iPhone 14 Pro")
     }
 }
+
--- StepByStep/Navigation/AddHabitsView.swift
+++ StepByStep/Navigation/AddHabitsView.swift
@@ -9,95 +9,114 @@ import SwiftUI
 
 struct AddHabitsView: View {
     
+    let selectedButton: String
+    
     //Properties
     var body: some View {
         
-        GeometryReader { geometry in
-        
-        //View
-        VStack {
-            
-            VStack {
-                Spacer().frame(height:80)
-                Text("Here are some suggestions to help you achieve your goal!")
-                    .font(.headline)
-                Spacer().frame(height:50)
-                Text("Tap one, then continue").font(.caption)
-                Spacer().frame(height:20)
-            }.lineSpacing(5)
-            
-            
-            //Buttons
-            VStack{
-                //Exercise regularly
-                Button(action: {
-                    
-                }, label: {
-                    Text("Exercise regularly")
-                }).buttonStyle(RoundedButtonStyle())
-                
-                Spacer()
-                
-                //Drink 2L water everyday
-                Button(action: {
-                    
-                }, label: {
-                    Text("Drink 2L water everyday")
-                }).buttonStyle(RoundedButtonStyle())
-                
-                Spacer()
-                
-                //Eat salad
-                Button(action: {
-                    
-                }, label: {
-                    Text("Eat salad")
-                }).buttonStyle(RoundedButtonStyle())
-                Spacer()
-            
-                
-            }
-            
-            
-            VStack {
-                Text("+")
-                Text("Or add your little step manually :)").font(.caption)
-                TextField("Write your response here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    .textFieldStyle(RoundedTextFieldStyle())
-               
-            }
-            
-            Spacer().frame(height:30)
-            
-            Button(action: {
-                
-            }, label: {
-                VStack {
-                    Image(systemName: "arrow.right.circle")
-                        .resizable()
-                        .frame(width:50,height:50)
-                    Text("Continue")
-                }
-            })
-           
-            Spacer().frame(height: 100)
-            
-        }
-        .padding()
-        
-
-        }
+            HabitsView(selectedButton: selectedButton)
         
     }
-    
-    //Methods
     
     
 }
 
 struct AddHabitsView_Previews: PreviewProvider {
     static var previews: some View {
-        AddHabitsView()
+        AddHabitsView(selectedButton: "Be a morning person")
             .previewDevice("iPhone 14 Pro")
     }
+}
+
+struct HabitsView: View {
+    
+    let selectedButton: String
+    
+    //Properties
+    var body: some View {
+        GeometryReader { geometry in
+        
+            
+            NavigationView{
+                VStack {
+                    
+                    let habits = setHabits(selectedButton: selectedButton)
+                        .components(separatedBy: ";")
+                    
+                    VStack {
+                        Spacer().frame(height:80)
+                        Text("Here are some suggestions to help you achieve your goal!")
+                            .font(.headline)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .multilineTextAlignment(.center)
+                        Spacer().frame(height:50)
+                        Text("Tap one, then continue").font(.caption)
+                        Spacer().frame(height:20)
+                    }.lineSpacing(5)
+                    
+                    
+                    //Buttons
+                    VStack{
+                        ForEach(habits, id: \.self) { habit in
+                            Button(action: {
+                                
+                            }, label: {
+                                Text(habit)
+                            }).buttonStyle(RoundedButtonStyle())
+                            
+                            Spacer()
+                        }
+                        
+                    }
+                    
+                    
+                    VStack {
+                        Text("+")
+                        Text("Or add your little step manually :)").font(.caption)
+                        TextField("Write your response here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                            .textFieldStyle(RoundedTextFieldStyle())
+                        
+                    }
+                    
+                    Spacer().frame(height:30)
+                    
+                    //need to add a navigation link here
+                    Button(action: {
+                        
+                    }, label: {
+                        VStack {
+                            Image(systemName: "arrow.right.circle")
+                                .resizable()
+                                .frame(width:50,height:50)
+                            Text("Continue")
+                        }
+                    })
+                    
+                    Spacer().frame(height: 100)
+                    
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+func setHabits (selectedButton: String) -> String {
+    switch selectedButton {
+    case "Staying fit":
+        return "Exercise regularly;Drink 2L water everyday;Eat salad"
+    case "Be a morning person":
+        return "Sleep early;Eat breakfast;Have a morning routine"
+    case "Learn something new":
+        return "Practice regularly;Connect with community;Listen to podcast"
+    case "Control my budget":
+        return "Review budget;Review spending habits;Transfer money to savings"
+    case "Stay mentally healthy":
+        return "Meditate;Talk with loved ones;Walk outside"
+    case "Be more productive":
+        return "Take regular breaks;Sleep early;Review task list before bed"
+    default:
+        return ""
+    }
+    
 }
