947d747b4139bd9cfbd3a2ae4154c9bd8260bd8d
Emily Deng <emily.x.deng@student.uts.edu.au>
2023-05-21 00:28:59 +1000

    Fixed issues with AddGoalView :(

--- StepByStep/Navigation/AddGoalView.swift
+++ StepByStep/Navigation/AddGoalView.swift
@@ -8,46 +8,27 @@
 import SwiftUI
 import Foundation
 
-struct AddGoalView: View {-
--
+struct AddGoalView: View {
     
     @State private var shouldShowAfterSettingGoalView = false
     
-    //Properties
+    // Properties
     var body: some View {
         NavigationView {
             VStack {
-                Spacer().frame(height:16)
-                    
+                Spacer().frame(height: 16)
+                
                 Text("Do you have something big you want to achieve?")
                     .font(.custom("Skia", size: 20))
-                    .offset(x:-10)
+                    .offset(x: -10)
                 
-                Spacer().frame(height:50)
+                Spacer().frame(height: 50)
                 
-                NavigationLink(
-                    destination: AddHabitsView(selectedGoal: "Staying fit")) {
-                        Button(action: {
-                        }, label: {
-                            HStack {
-                                Spacer()
-                                Image("dumbbell@4x")
-                                    .resizable()
-                                    .frame(width: 50, height: 30)
-                                Spacer()
-                                Text("Staying fit")
-                                    .font(.callout)
-                                    .foregroundColor(.black)
-                                Spacer()
-                            }
-                        }).buttonStyle(RoundedButtonStyle())
-
-                //Staying Fit
+                // Staying Fit
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
-                    
                 }, label: {
-                    HStack{
+                    HStack {
                         Spacer()
                         Image("dumbbell@4x")
                             .resizable()
@@ -58,9 +39,8 @@ struct AddGoalView: View {-
                             .foregroundColor(.black)
                         Spacer()
                     }
-                })
-                
-                //Be a morning person
+                }).buttonStyle(RoundedButtonStyle())
+                // Be a morning person
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
                 }, label: {
@@ -74,112 +54,14 @@ struct AddGoalView: View {-
                             .font(.callout)
                             .foregroundColor(.black)
                         Spacer()
-
                     }
-
-
-                //Be a morning person
-                NavigationLink(destination: AddHabitsView(selectedGoal: "Be a morning person")) {
-                    Button(action: {
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Image("sun@4x")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                            Spacer()
-                            Text("Be a morning person")
-                                .font(.callout)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                    }).buttonStyle(RoundedButtonStyle())
-                }
-
-                //Learn something new
-                NavigationLink(destination: AddHabitsView(selectedGoal: "Learn something new")) {
-                    Button(action: {
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Image("book@4x")
-                                .resizable()
-                                .frame(width: 30, height: 40)
-                            Spacer()
-                            Text("Learn something new")
-                                .font(.callout)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                    }).buttonStyle(RoundedButtonStyle())
-                }
-
-
-                //Control my budget
-                NavigationLink(destination: AddHabitsView(selectedGoal: "Control my budget")) {
-                    Button(action: {
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Image("money@4x")
-                                .resizable()
-                                .frame(width: 30, height: 40)
-                            Spacer()
-                            Text("Control my budget")
-                                .font(.callout)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                    }).buttonStyle(RoundedButtonStyle())
-                }
-
-
-                //Stay mentally healthy
-                NavigationLink(destination: AddHabitsView(selectedGoal: "Stay mentally healthy")) {
-                    Button(action: {
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Image("locked heart@4x")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                            Spacer()
-                            Text("Stay mentally healthy")
-                                .font(.callout)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                    }).buttonStyle(RoundedButtonStyle())
-                }
-
-
-                //Be more productive
-                NavigationLink(destination: AddHabitsView(selectedGoal: "Be more productive")) {
-                    Button(action: {
-
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Image("cogwheel@4x")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                            Spacer()
-                            Text("Be more productive")
-                                .font(.callout)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                    }).buttonStyle(RoundedButtonStyle())
-                }
-
-
-                Spacer()
-
-
+                }).buttonStyle(RoundedButtonStyle())
+                
+                // Learn something new
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
                 }, label: {
-                    HStack{
+                    HStack {
                         Spacer()
                         Image("book@4x")
                             .resizable()
@@ -190,13 +72,13 @@ struct AddGoalView: View {-
                             .foregroundColor(.black)
                         Spacer()
                     }
-                })
+                }).buttonStyle(RoundedButtonStyle())
                 
-                //Control my budget
+                // Control my budget
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
                 }, label: {
-                    HStack{
+                    HStack {
                         Spacer()
                         Image("money@4x")
                             .resizable()
@@ -207,13 +89,12 @@ struct AddGoalView: View {-
                             .foregroundColor(.black)
                         Spacer()
                     }
-                })
-                
-                //Stay mentally healthy
+                }).buttonStyle(RoundedButtonStyle())
+                // Stay mentally healthy
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
                 }, label: {
-                    HStack{
+                    HStack {
                         Spacer()
                         Image("locked heart@4x")
                             .resizable()
@@ -224,13 +105,13 @@ struct AddGoalView: View {-
                             .foregroundColor(.black)
                         Spacer()
                     }
-                })
+                }).buttonStyle(RoundedButtonStyle())
                 
-                //Be more productive
+                // Be more productive
                 Button(action: {
                     shouldShowAfterSettingGoalView = true
                 }, label: {
-                    HStack{
+                    HStack {
                         Spacer()
                         Image("cogwheel@4x")
                             .resizable()
@@ -241,24 +122,19 @@ struct AddGoalView: View {-
                             .foregroundColor(.black)
                         Spacer()
                     }
-                })
+                }).buttonStyle(RoundedButtonStyle())
                 
-             
+                Spacer()
             }
-                .padding()
-                .navigationTitle("Add New Goal")
-
-
-
-        }
-        .navigationBarHidden(true)
-        .fullScreenCover(isPresented: $shouldShowAfterSettingGoalView) { // Transition to AddGoalView
-            AfterSettingGoalView()
+            .padding()
+            .navigationTitle("Add New Goal")
+            .navigationBarHidden(true)
+            .fullScreenCover(isPresented: $shouldShowAfterSettingGoalView) {
+                AfterSettingGoalView()
+            }
         }
     }
-
 }
-
 
 struct AddGoalView_Previews: PreviewProvider {
     static var previews: some View {
@@ -266,4 +142,3 @@ struct AddGoalView_Previews: PreviewProvider {
             .previewDevice("iPhone 14 Pro")
     }
 }
-
