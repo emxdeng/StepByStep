08111f614de88a9039daee6199d144946ad9bb34
Janet <janet970527@gmail.com>
2023-05-23 12:42:00 +1000

    Refactor ContentView

--- StepByStep/Navigation/ContentView.swift
+++ StepByStep/Navigation/ContentView.swift
@@ -233,36 +233,6 @@ struct ContentView: View {
         }
 }
         
-        struct WeekdayPicker: View {
-            @Binding var selectedDays: [String]
-            @Binding var selectedHabitColor: Color?
-            
-            let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-            
-            var body: some View {
-                HStack(spacing: 8) {
-                    ForEach(weekdays, id: \.self) { weekday in
-                        Button(action: {
-                            if selectedDays.contains(weekday) {
-                                selectedDays.removeAll { $0 == weekday }
-                            } else {
-                                selectedDays.append(weekday)
-                            }
-                        }) {
-                            Text(weekday)
-                                .font(.system(size: 16))
-                                .fontWeight(.bold)
-                                .padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
-                                .foregroundColor(selectedDays.contains(weekday) ? .white : .black)
-                                .background(selectedDays.contains(weekday) ? (selectedHabitColor ?? Color.blue) : Color.clear)
-                                .cornerRadius(8)
-                                .fixedSize()
-                        }
-                    }
-                }
-            }
-        }
-    
         
         struct ContentView_Previews: PreviewProvider {
             static var previews: some View {
---
+++ StepByStep/Navigation/WeekdayPicker.swift
@@ -0,0 +1,39 @@
+//
+//  WeekdayPicker.swift
+//  StepByStep
+//
+//  Created by Janet on 23/5/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeekdayPicker: View {
+    @Binding var selectedDays: [String]
+    @Binding var selectedHabitColor: Color?
+    
+    let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ForEach(weekdays, id: \.self) { weekday in
+                Button(action: {
+                    if selectedDays.contains(weekday) {
+                        selectedDays.removeAll { $0 == weekday }
+                    } else {
+                        selectedDays.append(weekday)
+                    }
+                }) {
+                    Text(weekday)
+                        .font(.system(size: 16))
+                        .fontWeight(.bold)
+                        .padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
+                        .foregroundColor(selectedDays.contains(weekday) ? .white : .black)
+                        .background(selectedDays.contains(weekday) ? (selectedHabitColor ?? Color.blue) : Color.clear)
+                        .cornerRadius(8)
+                        .fixedSize()
+                }
+            }
+        }
+    }
+}
